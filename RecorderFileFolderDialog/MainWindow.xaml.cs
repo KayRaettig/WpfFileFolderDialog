c8ca0e715ad9fa9ebebe1ace62e51ad26611dcee
KaRae <KR>
2015-06-26 14:34:18 +0200

    tested something

--- RecorderFileFolderDialog/MainWindow.xaml.cs
+++ RecorderFileFolderDialog/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -32,10 +33,15 @@ namespace RecorderFileFolderDialog
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+
+            var wnd = new Window();
+            wnd.Show();
+            WindowCustomization.WindowExtension.Test(wnd);
+
+
+            //Dialog dlg = new Dialog();
             
-            Dialog dlg = new Dialog();
-            
-            dlg.ShowDialog();
+            //dlg.ShowDialog();
         }
     }
 }
